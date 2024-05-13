41795d4b1b80faa9561224b57ad1b08beb4933a0
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-05-13 21:46:01 +0530

    feat:setting up router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import JoinRoom from "./components/JoinRoom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Room from "./components/pages/Room";
 
 export default function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/:roomId" element={<Room/>} />
+
+    </Routes>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import SocketProvider from "./context/SocketContext.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <SocketProvider>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </SocketProvider>
 );
